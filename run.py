49039924219147a4948c10a390b2fe1ba175e3ed
sbentze <72312831+sbentze@users.noreply.github.com>
2021-06-10 13:25:47 +0200

    Made some changes with Berkay

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import logging
 
 import waitress
 
-from url_shortenr.config import Config, load_config
-from url_shortenr.logging import configure_logging
-from url_shortenr.main import make_app
+from url_shortener.config import Config, load_config
+from url_shortener.logging import configure_logging
+from url_shortener.main import make_app
 
 log = logging.getLogger(__name__)
 
