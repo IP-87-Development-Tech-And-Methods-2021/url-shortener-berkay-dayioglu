c3bd0449c87cbac9b4585a2b1a77a5ea9ec8b65d
sbentze <72312831+sbentze@users.noreply.github.com>
2021-06-10 22:22:40 +0200

    Initial logic functionality

--- url_shortener/logic.py
+++ url_shortener/logic.py
@@ -1,38 +1,60 @@
+import trafaret as t
+
 from threading import Lock
 from typing import Optional
+from os import urandom
 
-from url_shortener.storage import Storage
+from url_shortener.storage import PermanentStorage, InMemoryStorage
 from url_shortener.dto import User
 
 
 class Logic:
     """This class implements application logic"""
 
-    def __init__(self, storage: Storage):
-        """ Creating an instance of our logic. Pay attention that storage is not created within the logic, but
-        passed as a parameter from outside. This is called Dependency Injection.
-        """
-        super().__init__()
-        self._storage: Storage = storage
+    def __init__(self, storage: PermanentStorage, storage_mem: InMemoryStorage):
+        self._storage: PermanentStorage = storage
+        self._storage_mem: InMemoryStorage = storage_mem
         self._check_and_write_lock: Lock = Lock()
+        t_email = t.Email
+        t_pwd   = t.String(min_length=6, max_length=63)
 
-    def get_example(self, key: str) -> Optional[str]:
-        """Retrieves data from storage by key"""
-        return self._storage.read(key)
-
-    def save_example_if_not_exists(self, key: str, value: str) -> bool:
-        """ Tries to save value at given key.
-        Fails if key already has value in the storage.
-        Returns True if saving value was successful, otherwise False
-        """
-        # Acquire lock, so that we do not get ourselves into the data race
-        with self._check_and_write_lock:
-            existing = self._storage.read(key)
-            if existing is not None:
-                return False
-
-            self._storage.write(key, value)
+    # TODO: add logging
+    #       finish trafaret checking
+    def read_user(email: str):
+        return self._storage.read_user(email)
+    def add_user(email:str, password: str):
+        try:
+            t_email.check(email)
+            t_pwd.check(password)
+            self._storage.add_user(email, password)
             return True
+        except:
+            return False
+
+    def remove_user(email:str):
+        self._storage.remove_user(email, password)
+
+    def read_url(email:str, url_short: str):
+        self._storage.
+    def add_url(email:str, url_short: str, url_orig: str):
+        self._storage.add_url(email, url_short, url_orig)
+    def remove_url(email:str, url_short: str):
+        self._storage.remove_url(email, url_short)
+
+    def add_token(email: str):
+        tokens = self._storage_mem.read(email)
+        if tokens is not None:
+            return False
+        tokens[email] = urandom(128)
+        self._storage_mem.write(tokens)
+        return True
+    def remove_token(email: str): 
+        tokens = self._storage_mem.read(email)
+        if tokens is None:
+            return False
+        tokens.pop(email)
+        self._storage_mem.write(tokens)
+        return True
 
     def find_user_by_token(self, token: str) -> Optional[User]:
         # TODO: implement actual checking logic
