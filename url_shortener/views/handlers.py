91ab3fede58086b0ac74c30d91f2df6303891389
sbentze <72312831+sbentze@users.noreply.github.com>
2021-06-11 05:39:54 +0200

    Clean up example code

--- url_shortener/views/handlers.py
+++ url_shortener/views/handlers.py
@@ -53,63 +53,6 @@ def login_user(request: Request) -> Response:
         'status': 'token created',
         'token': token })
 
-
-def protected_resource_read_example(request: Request) -> Response:
-    key = request.matchdict['key']
-    logic: Logic = request.registry.logic
-    value = logic.get_example(key)
-
-    if value is None:
-        return Response(
-            status=httplib.NOT_FOUND,
-            json_body={
-                'status': 'error',
-                'reason': 'Resource does not exist'
-            },
-        )
-
-    return Response(
-        status=httplib.OK,
-        json_body={
-            'key': key,
-            'value': value,
-        },
-    )
-
-
-def protected_resource_write_example(request: Request) -> Response:
-    key = request.matchdict['key']
-    value = request.json_body.get('value')
-
-    if value is None:
-        return Response(status=httplib.BAD_REQUEST,
-                        json_body={
-                            'status': 'error',
-                            'reason':
-                            '`value` was not provided within request',
-                        })
-
-    logic: Logic = request.registry.logic
-    success = logic.save_example_if_not_exists(key, value)
-
-    if success:
-        return Response(
-            status=httplib.CREATED,
-            headerlist=[('Location',
-                         request.registry.base_url + '/resource/' + key)],
-        )
-
-    return Response(status=httplib.CONFLICT,
-                    json_body={
-                        'status': 'error',
-                        'reason': 'Resource already exists'
-                    })
-
-
-def public_resource_example(request: Request) -> Response:
-    return Response(status=httplib.OK, json_body={'available_for': 'everyone'})
-
-
 def notfound(request: Request) -> Response:
     return Response(status=httplib.NOT_FOUND, json_body={
         'status': 'error',
