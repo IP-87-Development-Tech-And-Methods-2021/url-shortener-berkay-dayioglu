d09f1269ab1db3820d355318f07cf3bc275ac7a6
sbentze <72312831+sbentze@users.noreply.github.com>
2021-06-11 05:41:36 +0200

    Clean up example code

--- url_shortener/views/routes.py
+++ url_shortener/views/routes.py
@@ -28,25 +28,6 @@ def setup_routes(config):
     config.add_view(login_user,
                     route_name='login_user')
 
-    # Add protected resources
-    # pass `factory=PROTECTED` to the `add_route` method
-    # in order to make this resource available for authenticated users only
-    config.add_route('protected_resource_write_example',
-                     request_method='PUT',
-                     pattern='/resource/{key}',
-                     factory=PROTECTED)
-    config.add_view(protected_resource_write_example,
-                    route_name='protected_resource_write_example',
-                    permission='write')
-
-    config.add_route('protected_resource_read_example',
-                     request_method='GET',
-                     pattern='/resource/{key}',
-                     factory=PROTECTED)
-    config.add_view(protected_resource_read_example,
-                    route_name='protected_resource_read_example',
-                    permission='read')
-
     # Add error views
     config.add_notfound_view(notfound)
     config.add_forbidden_view(forbidden)
